0170ed57f808334efd73f073dbaccdf045e72bbc
Rohit Singh Rathore <54764168+rohit0110@users.noreply.github.com>
2021-12-06 14:36:57 +0530

    Update mask_rcnn.py

--- mask_rcnn.py
+++ mask_rcnn.py
@@ -69,86 +69,7 @@ def histogram_equalization_check(img_in):
     equ = cv2.merge((equ_b, equ_g, equ_r))
     #print(equ)
     #cv2.imwrite('output_name.png', equ)
-    return img_out
-
-from os import listdir
-from xml.etree import ElementTree
-from numpy import zeros
-from numpy import asarray
-from mrcnn.utils import Dataset
- 
-class KAISTDataset(Dataset):
-	# load the dataset definitions
-	def load_dataset(self, dataset_dir, is_train=True):
-		# define one class
-		self.add_class("dataset", 1, "person")
-		images_dir = dataset_dir + '/images/'
-		annotations_dir = dataset_dir + '/annot/'
-		# find all images
-		for filename in listdir(images_dir):
-			# extract image id
-			image_id = filename[:-4]
-			img_path = images_dir + filename
-			ann_path = annotations_dir + image_id + '.xml'
-			# add to dataset
-			self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
- 
-	# extract bounding boxes from an annotation file
-	def extract_boxes(self, filename):
-		# load and parse the file
-		tree = ElementTree.parse(filename)
-		# get the root of the document
-		root = tree.getroot()
-		# extract each bounding box
-		boxes = list()
-		for box in root.findall('.//bndbox'):
-			xmin = round(float(box.find('xmin').text))
-			ymin = round(float(box.find('ymin').text))
-			xmax = round(float(box.find('xmax').text))
-			ymax = round(float(box.find('ymax').text))
-			coors = [xmin, ymin, xmax, ymax]
-			boxes.append(coors)
-		# extract image dimensions
-		width = int(root.find('.//size/width').text)
-		height = int(root.find('.//size/height').text)
-		return boxes, width, height
- 
-	# load the masks for an image
-	def load_mask(self, image_id):
-		# get details of image
-		info = self.image_info[image_id]
-		# define box file location
-		path = info['annotation']
-		# load XML
-		boxes, w, h = self.extract_boxes(path)
-		# create one array for all masks, each on a different channel
-		masks = zeros([h, w, len(boxes)], dtype='uint8')
-		# create masks
-		class_ids = list()
-		for i in range(len(boxes)):
-			box = boxes[i]
-			row_s, row_e = box[1], box[3]
-			col_s, col_e = box[0], box[2]
-			masks[row_s:row_e, col_s:col_e, i] = 1
-			class_ids.append(self.class_names.index('person'))
-		return masks, asarray(class_ids, dtype='int32')
- 
-	# load an image reference
-	def image_reference(self, image_id):
-		info = self.image_info[image_id]
-		return info['path']
- #/content/dataset/V000
-# train set
-train_set = KAISTDataset()
-train_set.load_dataset('/content/betaset', is_train=True)
-train_set.prepare()
-print('Train: %d' % len(train_set.image_ids))
- 
-# test/val set
-test_set = KAISTDataset()
-test_set.load_dataset('/content/betaset', is_train=False)
-test_set.prepare()
-print('Test: %d' % len(test_set.image_ids))
+    return img_out 
 
 image_id = 0
 image = train_set.load_image(image_id)
@@ -205,13 +126,6 @@ mask, class_ids = train_set.load_mask(image_id)
 bbox = extract_bboxes(mask)
 # display image with masks and bounding boxes
 display_instances(image, bbox, mask, class_ids, train_set.class_names)
-
-from mrcnn.config import Config
-class KAISTConfig(Config):
-  NAME = 'KAIST_cfg'
-  NUM_CLASSES = 1+1
-  STEPS_PER_EPOCH = 4400
-config = KAISTConfig()
 
 from mrcnn.model import MaskRCNN
 model = MaskRCNN(mode = 'training', model_dir = './' , config = config)
@@ -341,7 +255,7 @@ model = MaskRCNN(mode='training', model_dir='./', config=config)
 # load weights (mscoco) and exclude the output layers
 model.load_weights('/content/mask_rcnn_coco.h5', by_name=True, exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",  "mrcnn_bbox", "mrcnn_mask"])
 # train weights (output layers or 'heads')
-model.train(train_set, test_set, learning_rate=config.LEARNING_RATE, epochs=5, layers='heads')
+model.train(train_set, test_set, learning_rate=config.LEARNING_RATE, epochs=20, layers='heads')
 
 # define the prediction configuration
 class PredictionConfig(Config):
